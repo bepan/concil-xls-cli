41d834135dc293ee2b8f41fc569043bb1f9b2464
Alberto Chapa <e-achapar@neoris.com>
2019-12-16 19:42:38 -0600

    group by Aux and conciliate sorted by Concept

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
 const XLSX = require('xlsx');
 const path = require('path');
+var _ = require('lodash');
 
+var startEx = new Date();
 // Read Concil file
-const filePath = path.join(__dirname, '1105 30480 Comercio Otros deudores operaciones -Julio 2018.xlsb');
-const workbook = XLSX.readFile(filePath, {cellDates: true});
+const filePath = path.join(__dirname, 'Cuentas y Aux Nov 2019.xlsx');
+const workbook = XLSX.readFile(filePath);
 
 // Get the first Worksheet
 const firstSheetName = workbook.SheetNames[0];
@@ -12,44 +14,59 @@ const ws = workbook.Sheets[firstSheetName];
 // Set the range which the database starts at
 var range = XLSX.utils.decode_range(ws['!ref']);
 range.s.c = 0; // 0 == XLSX.utils.decode_col("A")
-range.s.r = 6;
+range.s.r = 1;
 // range.e.c = 6; // 6 == XLSX.utils.decode_col("G")
 // range.e.c = 6;
 var new_range = XLSX.utils.encode_range(range);
 
 // Parse and manip the data
-const concilData = XLSX.utils.sheet_to_json(ws, {range: new_range});
+let concilData = XLSX.utils.sheet_to_json(ws, {range: new_range});
+var grouped = _.groupBy(concilData, 'Aux');
+const groupedKeys = Object.keys(grouped);
+concilData = [];
+
 const matchesArr = [];
 const pendingRegs = [];
 const processedMap = new Map();
 
-var startEx = new Date();
-for (let currReg of concilData) 
+for (let aux of groupedKeys)
 {
-  if (processedMap.get(currReg)) continue;
-  let matchFound = false;
-  const amountCurrReg = currReg.CargoCG ? 'CargoCG' : 'AbonoCG';
-  const amountIterReg = amountCurrReg === 'CargoCG' ? 'AbonoCG' : 'CargoCG';
+  let oldConcept = '', iterStart = 0;
+  grouped[aux] = _.sortBy(grouped[aux], ['Concepto']);
 
-  for (let iterReg of concilData) 
+  for (let i = 0; i < grouped[aux].length; i++) 
   {
-    if (currReg === iterReg || processedMap.get(iterReg)) continue;
-    if (currReg.Concepto === iterReg.Concepto &&
-        currReg[amountCurrReg] === iterReg[amountIterReg]) 
+    const currReg = grouped[aux][i];
+    if (processedMap.get(currReg)) continue;
+    iterStart = currReg.Concepto !== oldConcept ? i : iterStart;
+    oldConcept = currReg.Concepto;
+    let matchFound = false;
+    const amountCurrReg = currReg.CargoCG ? 'CargoCG' : 'AbonoCG';
+    const amountIterReg = amountCurrReg === 'CargoCG' ? 'AbonoCG' : 'CargoCG';
+
+    for (let j = iterStart; j < grouped[aux].length; j++) 
     {
-      matchFound = true;
-      matchesArr.push(currReg, iterReg);
+      const iterReg = grouped[aux][j];
+      if (processedMap.get(iterReg)) continue;
+      if (iterReg.Concepto !== oldConcept) break;
+      if (currReg.Concepto === iterReg.Concepto && currReg[amountCurrReg] === iterReg[amountIterReg]) 
+      {
+        matchFound = true;
+        matchesArr.push(currReg, iterReg);
+        processedMap.set(currReg, true).set(iterReg, true);
+        break;
+      }
+    }
+
+    if (!matchFound)
+    {
       processedMap.set(currReg, true);
-      processedMap.set(iterReg, true);
-      break;
+      pendingRegs.push(currReg);
     }
   }
 
-  if (!matchFound)
-  {
-    processedMap.set(currReg, true);
-    pendingRegs.push(currReg);
-  }
+  delete grouped[aux];
+  processedMap.clear();
 }
 
 var endEx = new Date() - startEx;
